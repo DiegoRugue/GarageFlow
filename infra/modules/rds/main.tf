28078e22abf2f9393fc4e83940395830cf87c9c8
Diego Rugue <diegorugue@gmail.com>
2026-07-13 20:53:59 -0300

    fix: harden managed aws resources

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -41,7 +41,7 @@ resource "aws_db_instance" "this" {
   performance_insights_enabled = false
   deletion_protection          = false
   skip_final_snapshot          = true
-  backup_retention_period      = 0
+  backup_retention_period      = 7
   apply_immediately            = true
 
   db_name  = "garageflow"
